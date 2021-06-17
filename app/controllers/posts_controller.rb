c8aaaa1f2657c3f8668d86ca6ab8566a46d6e95f
kohei <shoot0321@gmail.com>
2021-06-17 15:47:55 +0900

    ページネーションを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  PER_PAGE = 10
+
   def index
-    @posts = Post.order(id: :desc)
+    @posts = Post.order("created_at DESC").page(params[:page]).per(PER_PAGE)
   end
 
   def create
